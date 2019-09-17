df7366a74203ccea69738ed25390986ac2efb2a3
Clinton Carter <clinton.carter2018@ou.edu>
2019-09-17 11:17:43 -0500

    strirn

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿//Clinton Carter
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your favorite word >>?");
+            string answer = Console.ReadLine().ToUpper().Trim();
+            string anotherString = "Something else";
+            string answerAsBIGLETTERS = answer.ToUpper();
+
+            for (int i = 0; i < answerAsBIGLETTERS.Length; i++)
+            {
+                Console.Write(answerAsBIGLETTERS[i] + " ");
+
+            }
+
+            Console.WriteLine($"Your favotie word has {answer.Length} characters in it");
+            
+
+            Console.ReadKey();
+        }
+    }
+}
